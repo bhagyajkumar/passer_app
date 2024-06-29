2a052ac331c44a126116d5208b23d7266b09c708
bhagyaj <bhagyajkumar1@gmail.com>
2024-06-29 22:05:44 +0530

    done with sharing ticket

--- lib/screens/event_detail_screen.dart
+++ lib/screens/event_detail_screen.dart
@@ -1,8 +1,15 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:passer/services/api_service.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:screenshot/screenshot.dart';
+import 'package:share_plus/share_plus.dart';
 import 'create_ticket_screen.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'dart:math';
+import 'dart:typed_data';
 
 class EventDetailScreen extends StatelessWidget {
   final int eventId;
@@ -58,6 +65,8 @@ class EventDetailScreen extends StatelessWidget {
     );
   }
 
+  ScreenshotController _screenshotController = ScreenshotController();
+
   // Function to show a dialog with ticket details
   Future<void> _showTicketDetailsDialog(BuildContext context, dynamic ticket) {
     return showDialog<void>(
@@ -66,25 +75,47 @@ class EventDetailScreen extends StatelessWidget {
         return AlertDialog(
           title: Text('Ticket Details'),
           content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                Text('Ticket ID: ${ticket['id']}'),
-                SizedBox(height: 10),
-                Text('Description: ${ticket['description']}'),
-                SizedBox(
-                  height: 320,
-                  width: 320,
-                  child: QrImageView(
-                    data: ticket['id'],
-                    version: QrVersions.auto,
-                    size: 320,
-                    gapless: false,
-                  ),
-                )
-              ],
+            child: Screenshot(
+              controller: _screenshotController,
+              child: ListBody(
+                children: <Widget>[
+                  Text('Ticket ID: ${ticket['id']}'),
+                  SizedBox(height: 10),
+                  Text('Description: ${ticket['description']}'),
+                  SizedBox(
+                    height: 320,
+                    width: 320,
+                    child: QrImageView(
+                      data: ticket['id'],
+                      version: QrVersions.auto,
+                      size: 320,
+                      gapless: false,
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
           actions: <Widget>[
+            TextButton(
+              onPressed: () async {
+                await _screenshotController
+                    .capture(delay: const Duration(milliseconds: 10))
+                    .then((Uint8List? image) async {
+                  // Note the change to Uint8List?
+                  if (image != null) {
+                    final directory = await getApplicationDocumentsDirectory();
+                    final imagePath =
+                        await File('${directory.path}/image.png').create();
+                    await imagePath.writeAsBytes(image);
+
+                    final imageXFile = XFile('${directory.path}/image.png');
+                    await Share.shareXFiles([imageXFile]);
+                  }
+                });
+              },
+              child: Text("Share"),
+            ),
             TextButton(
               child: Text('Close'),
               onPressed: () {
